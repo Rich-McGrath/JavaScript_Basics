721b8a9e8a0b4130a516127b13181745d963b12f
Rich-McGrath <richmcgrath@gmail.com>
2016-05-15 17:19:28 -0400

    Using var

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-alert("Hello From Rich");
-document.write("<h1>Welcome to JavaScript Basics</h1>");
-alert("Thanks for visiting");
-console.log("Program complete");
+// Made In active to follow along with next code example
+//alert("Hello From Rich");
+//document.write("<h1>Welcome to JavaScript Basics</h1>");
+//alert("Thanks for visiting");
+//console.log("Program complete");
+
+//New Code example using variables
+var message = "Hello!";
+alert(message);
