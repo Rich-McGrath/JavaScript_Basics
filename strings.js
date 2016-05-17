b5012825470596adbc7b26b0fe7233c130301550
Rich-McGrath <richmcgrath@gmail.com>
2016-05-16 21:39:02 -0400

    parseFloat example

--- strings.js
+++ strings.js
@@ -1,4 +1,7 @@
 var HTMLBadges = prompt('How many HTML badges do you have?');
 var CSSBadges = prompt('How many CSS Badges do you have?');
+//parseInt for Integers - parseFloat for Floating (Decimal) point
 var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
 alert('Wow! You have ' + totalBadges + ' badges!');
+
+console.log(parseFloat('3.14'));
