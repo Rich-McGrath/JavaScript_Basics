caefc1529c4a077d777e74e5105e68333f61a709
Rich-McGrath <richmcgrath@gmail.com>
2016-05-16 21:47:50 -0400

    Mad Libs Challenge Revisited

--- story.js
+++ story.js
@@ -1,8 +1,17 @@
+var questions= 4;
+var questionsLeft = '[ ' + questions + ' questions left] ';
 document.write("<h1>Crazy Story Creator</h1>");
-var profession = prompt("Please enter in a profession?");
-var name = prompt("Please enter a name?");
-var food = prompt("Please enter in a food");
-var place = prompt("Please enter in a place");
+var profession = prompt("Please enter in a profession? " + questionsLeft )
+questions -= 1;
+questionsLeft = '[ ' + questions + ' questions left] ';
+var name = prompt("Please enter a name? " + questionsLeft);
+questions -= 1;
+questionsLeft = '[ ' + questions + ' questions left] ';
+var food = prompt("Please enter in a food " + questionsLeft);
+questions -= 1;
+questionsLeft = '[ ' + questions + ' questions left] ';
+var place = prompt("Please enter in a place " + questionsLeft);
+
 
 var story = "Once upont a time there was a " + profession + ". Who needed to find Queen " + name +
               ". The " + profession + " ate magic " + food + " and was transported to a magical " + place + "...";
