4937cf87fabb5ec98554989aa78c727195544119
Rich-McGrath <richmcgrath@gmail.com>
2016-05-16 21:23:35 -0400

    Time Calculator

---
+++ math.js
@@ -0,0 +1,10 @@
+document.write("<h1>Time Calculator</h1>");
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write("There are " + secondsPerDay + " seconds in a day");
+var yearsAlive = 32;
+document.write("I have been alive for" + yearsAlive * secondPerDay + " seconds!");
