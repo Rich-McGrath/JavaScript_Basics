79591f3badde9931ba96b3940aebea80e3bce37a
Rich-McGrath <richmcgrath@gmail.com>
2016-05-16 21:34:53 -0400

    Adding Strings not numbers

---
+++ strings.js
@@ -0,0 +1,4 @@
+var HTMLBadges = prompt('How many HTML badges do you have?');
+var CSSBadges = prompt('How many CSS Badges do you have?');
+var totalBadges = HTMLBadges + CSSBadges;
+alert('Wow! You have ' + totalBadges + ' badges!');
