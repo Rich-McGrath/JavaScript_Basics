1439b9982dd6408269a97dffd88c74765cd4d824
Rich-McGrath <richmcgrath@gmail.com>
2016-05-18 13:49:33 -0400

    Random number challenge with parameters

--- random.js
+++ random.js
@@ -1,7 +1,15 @@
-function getRandomNumber( upper ) {
-  var randomNumber = Math.floor( Math.random() * upper ) + 1;
-  return randomNumber;
+function getRandomNumber( upper, lower ) {
+  return Math.floor( Math.random() * (upper - lower + 1) ) + lower;
 }
+
+//var lower = prompt("Please in a number.");
+//var upper = prompt("Please enter in a second number large than your previous number.");
+
+console.log( getRandomNumber ( 1, 6) );
+console.log( getRandomNumber ( 1, 1000) );
+console.log( getRandomNumber ( 200, 500) );
+console.log( getRandomNumber ( 1000, 20000) );
+console.log( getRandomNumber ( 50, 100) );
 
 /*
 console.log( getRandomNumber(6) );
@@ -10,9 +18,11 @@ console.log( getRandomNumber(10000) );
 console.log( getRandomNumber(2) );
 */
 
+/*
 function getArea(width, length, unit) {
   var area = width * length;
   return area + " " + unit;
 }
 
 console.log( getArea(10, 20, 'sq ft') );
+*/
