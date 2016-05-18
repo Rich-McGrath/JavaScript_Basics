a44ba7aa70b9b1b5f40d0c6244094d0aa723f79b
Rich-McGrath <richmcgrath@gmail.com>
2016-05-18 12:31:25 -0400

    Area of a Triangel Function example

--- random.js
+++ random.js
@@ -2,7 +2,17 @@ function getRandomNumber( upper ) {
   var randomNumber = Math.floor( Math.random() * upper ) + 1;
   return randomNumber;
 }
+
+/*
 console.log( getRandomNumber(6) );
 console.log( getRandomNumber(100) );
 console.log( getRandomNumber(10000) );
 console.log( getRandomNumber(2) );
+*/
+
+function getArea(width, length) {
+  var area = width * length;
+  return area;
+}
+
+console.log( getArea(10, 20) );
