ac760c635beb2976460cf76cd160d4fe7e597e89
Rich-McGrath <richmcgrath@gmail.com>
2016-05-18 13:56:02 -0400

    How to check and throw errors example

--- random.js
+++ random.js
@@ -1,15 +1,18 @@
 function getRandomNumber( upper, lower ) {
+  if (isNaN(lower) || isNaN(upper) ){
+    throw new Error('Both arugments must be numbers');
+  }
   return Math.floor( Math.random() * (upper - lower + 1) ) + lower;
 }
 
 //var lower = prompt("Please in a number.");
 //var upper = prompt("Please enter in a second number large than your previous number.");
 
-console.log( getRandomNumber ( 1, 6) );
+console.log( getRandomNumber ( 'nine', 6) );
 console.log( getRandomNumber ( 1, 1000) );
 console.log( getRandomNumber ( 200, 500) );
 console.log( getRandomNumber ( 1000, 20000) );
-console.log( getRandomNumber ( 50, 100) );
+console.log( getRandomNumber ( 50, 'one-hundred') );
 
 /*
 console.log( getRandomNumber(6) );
